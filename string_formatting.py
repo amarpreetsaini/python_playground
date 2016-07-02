051fd08b842951e7b083fd9d54c8ca0efd03306f
Amarpreet Singh <amarpreetsaini@live.com>
2016-07-02 08:55:50 +0530

    Update string_formatting.py

--- string_formatting.py
+++ string_formatting.py
@@ -2,3 +2,10 @@ string_1 = "Camelot"
 string_2 = "place"
 
 print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
+
+name = raw_input("What is your name?")
+quest = raw_input("What is your quest?")
+color = raw_input("What is your favorite color?")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
