f739fa65017ab19f2f05048c65c66d41ea6b40ef
Amarpreet Singh <amarpreetsaini@live.com>
2016-07-03 13:05:11 +0530

    Update typecheck.py

--- typecheck.py
+++ typecheck.py
@@ -1,7 +1,7 @@
 
   """  First, def a function called distance_from_zero, with one argument (choose any argument name you like).
     If the type of the argument is either int or float, the function should return the absolute value of the function input.
-    Otherwise, the function should return "Nope
+    Otherwise, the function should return "Nope" """
     
     def distance_from_zero(arg):
     if type(arg) == int or type(arg) == float:
